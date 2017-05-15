07207c5fcf7a892cd4a32f88032bb284fd1f71d2
Chetan Meshram <chmeshra@visa.com>
2017-05-15 13:15:16 -0700

    adding mocha tests for running axe

---
+++ test/axe.spec.js
@@ -0,0 +1,28 @@
+const assert = require("chai").assert;
+const axeBuilder = require("axe-webdriverjs");
+const webDriver =  require("selenium-webdriver");
+
+const driver = new webDriver.Builder()
+    .forBrowser('phantomjs')
+    .build();
+
+describe('aXe test', () => {
+    it('should check the main page of Consumer',  () => {
+        // a Mocha test case can be treated as asynchronous
+        // by returning a promise
+        return driver.get('http://sitepoint.com/')
+            .then(() => {
+                return new Promise((resolve) => {
+                    axeBuilder(driver).analyze((results)=>{
+                        assert.equal(results.violations.length, 0);
+
+                        resolve();
+                    });
+                });
+            })
+            .then(() => driver.quit())
+    })
+    // The test might take some 5-10 seconds to execute,
+    // so we'lldisable the timeout
+    .timeout(0);
+});
