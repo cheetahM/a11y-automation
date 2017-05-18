5209e85355010dc82d80be3f64c9db656efd819f
Chetan Meshram <chmeshra@visa.com>
2017-05-18 11:10:50 -0700

    axe changes

--- axe.js
+++ axe.js
@@ -3,15 +3,30 @@ const webDriver = require("selenium-webdriver");
 
 // create a PhantomJS WebDriver instance
 const driver = new webDriver.Builder()
- .forBrowser('firefox')
- .build();
+ .forBrowser('chrome');
+
+const browser = driver.build();
+
+browser.manage().timeouts().setScriptTimeout(60000);
 
 // run the tests and output the results in the console
-driver
+/*browser
  .get("https://vbox132.secure.checkout.visa.com/")
  .then(() => {
     axeBuilder(driver)
      .analyze((results) => {
         console.log("Results are: ", results);
     });
-});
+});*/
+browser
+  .get("https://vbox132.secure.checkout.visa.com/")
+  .then(() => {
+      axeBuilder(browser)
+     .analyze((results) => {
+        //  const foo = JSON.stringify(results);
+         console.log(results.violations[0]);
+    });
+  });
+
+browser
+ .quit();
